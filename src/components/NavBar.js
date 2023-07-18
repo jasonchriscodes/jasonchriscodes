0131310944dc8d9da2b16cb87c571b766a4e941a
mrb34n135421234 <jasonchristian1234@gmail.com>
2023-07-18 17:54:54 +1200

    chore: add navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,124 @@
+import React from "react";
+import { useEffect, useState } from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import logo from "../assets/img/logo.png";
+import navIcon1 from "../assets/img/github.svg";
+import navIcon2 from "../assets/img/linkedin.svg";
+import navIcon3 from "../assets/img/instagram.svg";
+import navIcon4 from "../assets/img/facebook.svg";
+
+const NavBar = () => {
+  const [activeLink, setActiveLink] = useState("home");
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", onScroll);
+
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
+
+  const onUpdateActiveLink = (value) => {
+    setActiveLink(value);
+  };
+
+  return (
+    <Navbar expand="lg" className={scrolled ? "scrolled" : ""}>
+      <Container>
+        <Navbar.Brand href="#home">
+          <img src={logo} alt="Logo" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav">
+          <span classNem="navbar-toggler-icon"></span>
+        </Navbar.Toggle>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link
+              href="#home"
+              className={
+                activeLink === "home" ? "active navbar-link" : "navbar-link"
+              }
+              onClick={() => onUpdateActiveLink("home")}
+            >
+              Home
+            </Nav.Link>
+            <Nav.Link
+              href="#skills"
+              className={
+                activeLink === "skills" ? "active navbar-link" : "navbar-link"
+              }
+              onClick={() => onUpdateActiveLink("skills")}
+            >
+              Skills
+            </Nav.Link>
+            <Nav.Link
+              href="#works"
+              className={
+                activeLink === "works" ? "active navbar-link" : "navbar-link"
+              }
+              onClick={() => onUpdateActiveLink("works")}
+            >
+              Works
+            </Nav.Link>
+            <Nav.Link
+              href="#projects"
+              className={
+                activeLink === "projects" ? "active navbar-link" : "navbar-link"
+              }
+              onClick={() => onUpdateActiveLink("projects")}
+            >
+              Projects
+            </Nav.Link>
+            <Nav.Link
+              href="#testimonials"
+              className={
+                activeLink === "testimonials"
+                  ? "active navbar-link"
+                  : "navbar-link"
+              }
+              onClick={() => onUpdateActiveLink("testimonials")}
+            >
+              Testimonials
+            </Nav.Link>
+            <Nav.Link
+              href="#contact"
+              className={
+                activeLink === "contact" ? "active navbar-link" : "navbar-link"
+              }
+              onClick={() => onUpdateActiveLink("contact")}
+            >
+              Contact
+            </Nav.Link>
+          </Nav>
+          <span className="navbar-text">
+            <div className="social-icon">
+              <a href="#">
+                <img src={navIcon1} alt="" />
+              </a>
+              <a href="#">
+                <img src={navIcon2} alt="" />
+              </a>
+              <a href="#">
+                <img src={navIcon3} alt="" />
+              </a>
+              <a href="#">
+                <img src={navIcon4} alt="" />
+              </a>
+            </div>
+            <button className="" onClick={() => console.log("connect")}>
+              <span>Let's Connect</span>
+            </button>
+          </span>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+export default NavBar;
