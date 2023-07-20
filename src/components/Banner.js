6f2dca6cc420cc065056574cf7a844a903cee52d
mrb34n135421234 <jasonchristian1234@gmail.com>
2023-07-20 16:19:34 +1200

    fix: image banner when loading

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -49,7 +49,7 @@ const Banner = () => {
     <section className="banner" id="home">
       <Container>
         <Row className="align-items-center">
-          <Col xs={12} md={6} xl={7}>
+          <Col className="text-left" xs={12} md={6} xl={7}>
             <span className="tagline">Welcome to my Portfolio</span>
             <h1>
               {`Hi I'm Jason Christian `}
